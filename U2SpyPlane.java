2982b2c7fbab2eece780fd2087f577d3f2c83742
Maria Bea Nicole Nocum <121146748+Mariyangge@users.noreply.github.com>
2023-05-29 15:32:23 +0800

    Delete U2SpyPlane.java

--- U2SpyPlane.java
+++
@@ -1,7 +0,0 @@
-public class U2SpyPlane extends Plane {
-    String wings_span;
-
-    public U2SpyPlane(String name, String color, int speed, String wings_span, double price) {
-        super(name, color, speed, wings_span, price);
-    }
-}
