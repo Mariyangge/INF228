06d4beb1de64d1a19f27dbafbf198544fc649779
Maria Bea Nicole Nocum <121146748+Mariyangge@users.noreply.github.com>
2023-05-29 15:32:17 +0800

    Delete ToyotaVIOS.java

--- ToyotaVIOS.java
+++
@@ -1,7 +0,0 @@
-public class ToyotaVIOS extends Car { 
-    String tire_type;
-
-    public ToyotaVIOS(String name, String color, int speed, String tire_type, double price) {
-        super(name, color, speed, tire_type, price);
-    }
-}
