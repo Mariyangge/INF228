605a0191915caceb25a05660c73b08da194d4185
Maria Bea Nicole Nocum <121146748+Mariyangge@users.noreply.github.com>
2023-05-29 15:32:04 +0800

    Delete FandangoYacht.java

--- FandangoYacht.java
+++
@@ -1,7 +0,0 @@
-public class FandangoYacht extends Boat {
-    String MainSail_color;
-
-    public FandangoYacht(String name, String color, int speed, String MainSail_color, double price){
-        super (name, color, speed, MainSail_color, price);
-    }
-}
